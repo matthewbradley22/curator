94a071d9fedb831cba7eeb8333500a57b70a0d59
matthewbradley22 <mbradley22@utexas.edu>
2022-07-25 14:30:56 -0500

    Initial project version

---
+++ R/prep_data.R
@@ -0,0 +1,58 @@
+#' retrieve records from gbif, wrapper around occ_search
+#'
+#' @param startingMonth Starting month of starting year for retrieving data
+#' @param startingYear Starting year for retrieving data
+#' @param name Scientific name from the GBIF backbone
+#' @param taxKey Gbif taxon  key
+#' @param genKey Gbif genus key
+#' @param numObs How many observations should be pulled from Gbif
+#' @param coordinate Return only records with lat and lon data
+#'
+#' @return returns a dataframe with biological data from Gbif
+#' @export
+#'
+#' @examples get_records_since_date()
+get_records_since_date <- function(startingMonth = 6,
+                                   startingYear = 2021,
+                                   name = "Carex",
+                                   taxKey = NULL,
+                                   genKey = NULL,
+                                   numObs = 500,
+                                   coordinate = TRUE){
+
+  curYear = format(Sys.Date(), "%Y")
+  rangeYears = paste(startingYear, curYear, sep = ", ")
+
+  search = rgbif::occ_search(scientificName = name,
+                      year = rangeYears,
+                      limit = numObs,
+                      hasCoordinate = coordinate,
+                      taxonKey = taxKey,
+                      genusKey = genKey)
+  data <- search$data
+  data <- data[!(data$year == startingYear & data$month < startingMonth),]
+  data$countryCode <-  countrycode::countrycode(data$countryCode, origin =  'iso2c', destination = 'iso3c')
+  data <- data[!is.na(data$species),]
+  return(data)
+}
+
+
+
+#' flag data using coordinate_cleaner
+#'
+#' @param data biological data to be flagged
+#'
+#' @return returns a dataframe with included flags
+#' @export
+#'
+#' @examples flag_data(data)
+flag_data <- function(data){
+
+  flags <- CoordinateCleaner::clean_coordinates(x = data,
+                             lon = "decimalLongitude",
+                             lat = "decimalLatitude",
+                             countries = "countryCode",
+                             species = "species")
+
+  return(flags)
+}
