6b596e8add9592a5fe1f7f3b8a3d963f45355f60
matthewbradley22 <mbradley22@utexas.edu>
2022-07-28 09:15:49 -0500

    new commit

--- R/data.R
+++ R/data.R
@@ -6,6 +6,20 @@
 #' @format A data frame with 985 rows and 148 variables:
 #' \describe{
 #'   \item{scientificName}{scientific name from gbif}
+#'   \item{decimalLatitude}{Latitude coordinates in decimal degrees}
+#'   \item{decimalLongitude}{Longitude coordinates in decimal degrees}
+#'   \item{species}{Genus and species epithet}
+#'   \item{countryCode}{ISO3 ccoutry code}
+#'   \item{.val}{Checks if coordinates correspond to a lat/lon coordinate reference system. True indicates no flag}
+#'   \item{.equ}{Tests for equal absolute longitude and latitude. True indicates no flag}
+#'   \item{.zer}{scientific name from gbif}
+#'   \item{.cap}{scientific name from gbif}
+#'   \item{.cen}{scientific name from gbif}
+#'   \item{.sea}{scientific name from gbif}
+#'   \item{.otl}{scientific name from gbif}
+#'   \item{.gbf}{scientific name from gbif}
+#'   \item{.inst}{scientific name from gbif}
+#'   \item{.summary}{scientific name from gbif}
 #'   ...
 #' }
 #' @source \url{https://www.gbif.org/occurrence/search}
@@ -19,6 +33,11 @@
 #' @format A data frame with 985 rows and 148 variables:
 #' \describe{
 #'   \item{scientificNameORpreviousID}{scientific name from gbif}
+#'   \item{species}{scientific name from gbif}
+#'   \item{decimalLongitude}{scientific name from gbif}
+#'   \item{scientificNameORpreviousID}{scientific name from gbif}
+#'   \item{decimalLatitude}{scientific name from gbif}
+#'   \item{CountryCode}{scientific name from gbif}
 #'   ...
 #' }
 #' @source \url{https://www.gbif.org/occurrence/search}
