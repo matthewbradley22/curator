e1c628cca621d7e1547546a04334909a5fcdc6b8
matthewbradley22 <mbradley22@utexas.edu>
2022-08-03 09:25:02 -0500

    new commit

---
+++ R/edit_data.R
@@ -0,0 +1,15 @@
+#' Remove any flagged data from dataset
+#'
+#' @param data name of dataset
+#' @param sumName name of column that summarizes flags, ie row values in column indicates if data is flagged or not
+#'
+#' @return dataset of the same type passed to it, but with all flagged data removed
+#' @export
+#'
+#' @examples
+#'
+rm_flag_data <- function(data,
+                         sumName = ".summary"){
+  newDat = data[data[[sumName]] == T,]
+  return(newDat)
+}
