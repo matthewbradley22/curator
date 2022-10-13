c0320f681884999ce84949d61d84482730040e20
matthewbradley22 <mbradley22@utexas.edu>
2022-10-13 10:23:24 -0500

    new commit

--- R/intoduced_flags.R
+++ R/intoduced_flags.R
@@ -2,6 +2,7 @@
 #'
 #' @param data dataset
 #' @param introCol name of column that contains introduced species labels
+#' @param introTitle Label of introduced species
 #'
 #' @return dataset of the same class passed to it. New column of introduced flags
 #' @export
@@ -13,5 +14,25 @@ flag_introduced <- function(data,
   introduced = data[introCol] != introTitle
   data$.introduced = introduced
   data[data$.introduced == FALSE, ".summary"] = FALSE
+  return(data)
+}
+
+
+#' Flag species labeled as "deleted"
+#'
+#' @param data dataset
+#' @param delCol name of column that contains introduced species labels
+#' @param delTitle Label of deleted species
+#'
+#' @return dataset of the same class passed to it. New column of introduced flags
+#' @export
+#'
+#' @examples
+flag_deleted <- function(data,
+                         delCol = "databaseChanges",
+                         delTitle = "deleted"){
+  deleted = data[delCol] != delTitle
+  data$.deleted = deleted
+  data[data$.deleted == FALSE, ".summary"] = FALSE
   return(data)
 }
